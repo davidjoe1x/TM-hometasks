dc66508fbf8bb81ac1412cc8b04c804359998889
davidjoe1x <david.zograbian@mail.ru>
2023-02-13 21:42:57 +0300

    hw1

---
+++ hw1.js
@@ -0,0 +1,127 @@
+
+
+//  function Summ(x, y, z) {
+//     let Summ = x + y + z;
+//     console.log(Summ);
+//  }
+//  Summ(20, 58, 42);
+
+
+
+
+
+// function showAge(m, s, d, h, age) {
+//     let myAgeInSeconds = d * age * h * m * s; 
+//     console.log(myAgeInSeconds);
+//     return myAgeInSeconds;
+
+    
+// }
+
+// showAge(60, 60, 365, 24, 24);
+
+
+
+
+
+
+// let count = 42
+// count= String(count);
+// console.log(count, typeof(count));
+
+// let userName = '42'
+// userName = Number(userName);
+// console.log(userName, typeof(userName));
+
+
+
+
+// function bBear(a, b, c) {
+//     let myBear = a + b + c;
+//     console.log(myBear);
+// }
+
+// bBear('1', '2 ', 'белых медведей');
+
+
+
+
+
+// function cCounter(s, s1, s2, s3, s4) {
+// lengthWords = s.length+s1.length+s2.length+s3.length+s4.length;
+// console.log(lengthWords);
+// }
+
+// cCounter('доступ', 'морпех', 'наледь', 'попрек', 'рубило')
+
+
+
+// let a = 222;
+// let b = "text";
+// let c = 1234n;
+// let f = ` Variable: %variable name% have type: %type variable%`;
+// console.log(a + f, typeof(a), b + f,typeof(b), c + f ,typeof(c));
+
+
+
+
+
+
+// let a = 4;
+// let b = 3;
+
+// console.log(a,b);
+
+// [a,b] = [b,a];
+
+// console.log(a,b);
+
+
+
+// // function myCipher($1, $2, $3, $4, $5, $6) {
+// // console.log($1.substr(1,1), $2.substr(1,1), $3.substr(1,1), $4.substr(1,1), $5);
+
+
+// // }
+
+// // myCipher('обернись', 'неужели', 'огурцы', 'липкие', '?!');
+
+
+
+// // let yourName = prompt('Как Вас зовут?');
+// // console.log(yourName);
+// // alert(`Ваc зовут ${yourName}!`);
+// // let age = prompt('Укажите возраст', 0);
+// // console.log(age);
+// // alert(`Вам ${age} лет!`);
+
+
+
+
+
+
+
+
+
+
+
+// // let floors = 28;
+// // for (let i = 1; i <= floors; i++) {
+// //     if (i == 13) {
+// //         continue;
+// //     }
+
+// // document.writeln("At floor: "+ i + "<br>");
+// // }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     let div = document.createElement('div');
+//     div.id = 'container';
+//     div.innerHTML = 'Hi there!';
+//     div.className = 'border pad';
+ 
+//     document.body.appendChild(div);
+// }, false);
+
+
+
